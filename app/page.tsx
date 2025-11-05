9fe9b2d9978e1e23df532697039610080426e417
Ascent817 <vapradeep817@gmail.com>
2025-11-04 16:26:03 -0800

    Add answer in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
+
+  let answer: number = 0;
+  answer = 3 + 5;
+
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
+      <p>The answer is: {answer}</p>
     </div>
   );
 }
