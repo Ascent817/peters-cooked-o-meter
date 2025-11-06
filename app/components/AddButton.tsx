aa6a1742a2ee7a042f72f9f3ed958c6df76eb5e3
Sophie Choi <103692454+destructivebasket@users.noreply.github.com>
2025-11-06 13:38:45 -0800

    calculate is broken for now

--- app/components/AddButton.tsx
+++ app/components/AddButton.tsx
@@ -2,12 +2,17 @@
 import "./Main.css"
 import CalculateButton from "./CalculateButton";
 import Inputs from "./Inputs";
-import { useState } from 'react';
+import { useState, useEffect, use } from 'react';
+import { Rating } from "../types/teacher";
 
 export default function AddButton () {
   const [classList, setClassList] = useState<number[]>([]);
   const [count, setCount] = useState(1);
+
+  const [canCalculate, setCanCalculate] = useState(false);
   const [showScore, setShowScore] = useState(false);
+  const [ratingList, setRatingList] = useState<Rating[]>([]);
+  const [totalRatings, setTotalRatings] = useState<number>(0);
 
   function addInput (count: number) {
     setClassList((prev) => [...prev, count]);
@@ -18,11 +23,12 @@ export default function AddButton () {
     <div>
       <div className="button-row">
         <button className="add-button" onClick = {() => addInput(count)} > Add Class </button>
-        <CalculateButton setShowScore = {setShowScore}/>
+        <CalculateButton showScore = {showScore} ratingList = {ratingList} totalRatings = {totalRatings} setTotalRatings = {setTotalRatings} setShowScore = {setShowScore} canCalculate = {canCalculate}/>
       </div>
+
       <div className="class-container">
         {classList.map((id) => {
-          return <Inputs key={id} id = {id} setClassList = {setClassList} showScore = {showScore}/>;
+          return <Inputs key={id} id = {id} setClassList = {setClassList} setShowScore = {setShowScore} setRatingList = {setRatingList} setCanCalculate = {setCanCalculate}/>;
         })}
       </div>
     </div>
--- app/components/CalculateButton.tsx
+++ app/components/CalculateButton.tsx
@@ -1,22 +1,42 @@
 interface CalculateButtonProps {
+  showScore: boolean;
+  ratingList: Rating[];
+  totalRatings: number;
+  setTotalRatings: React.Dispatch<React.SetStateAction<number>>;
   setShowScore: React.Dispatch<React.SetStateAction<boolean>>;
+
+  canCalculate: boolean;
 }
 
-import { useState } from 'react';
+import { Rating } from "../types/teacher";
 import "./Main.css"
 
-export default function CalculateButton ({setShowScore}: CalculateButtonProps) {
+export default function CalculateButton ({showScore, ratingList, totalRatings, setTotalRatings, setShowScore, canCalculate}: CalculateButtonProps) {
+  function calculateRatings() {
+    if (!canCalculate) return;
+
+    console.log(ratingList[0].difficultyRating);
+    //const total = allRatings.reduce((sum, r) => sum + r.node!.difficultyRating, 0);
+    //const average = total / allRatings.length;
+
+    //setTotalRatings(average);
+    setShowScore(true);
+
+    //console.log("allRatings:", allRatings);
+    //console.log("Average difficulty:", average);
+  }
 
   return (
     <div>
       <button 
         type="button" 
         className="calc-button"
-        onClick = {() => setShowScore(true)}
+        onClick = {() => calculateRatings()}
       >
         Calculate
       </button>
 
+    {showScore && <h1>{totalRatings}</h1>}
     </div>
   )
 }
