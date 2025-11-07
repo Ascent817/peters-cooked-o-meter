a246f10f3ef8d814d4d95f34194f311f804501cd
Sophie Choi <103692454+destructivebasket@users.noreply.github.com>
2025-11-06 21:25:35 -0800

    changed the algorithm

--- app/components/Score.tsx
+++ app/components/Score.tsx
@@ -10,18 +10,18 @@ export default function Score ({showScore, average}: ScoreProps) {
       {showScore && <div> {average} </div>}
       <div className="meter-box">
         {!showScore && <img src="/Peter2.svg" alt="Peter"></img>}
-        {average == 0 && showScore && <img src="/Peter2.svg" alt="Peter"></img>}
+        {average >= 0 && average < 2 && showScore && <img src="/Peter2.svg" alt="Peter"></img>}
         {average == 1 && showScore && <img src="/Peter1.svg" alt="Peter"></img>}
         {average == 2 && showScore && <img src="/Peter1.svg" alt="Peter"></img>}
         {average == 3 && showScore && <img src="/Peter2.svg" alt="Peter"></img>}
         {average == 4 && showScore && <img src="/Peter3.svg" alt="Peter"></img>}
-        {average == 5 && showScore && <img src="/Peter4.svg" alt="Peter"></img>}
+        {average >= 5 && showScore && <img src="/Peter4.svg" alt="Peter"></img>}
       </div>
-      {average == 1 && showScore && <div className="cooked-img"><img src="/Cooked1.svg" alt="Peter"/>1/5 This will be a well seasoned quarter!</div>}
+      {average >= 0 && average < 2 && showScore && <div className="cooked-img"><img src="/Cooked1.svg" alt="Peter"/>1/5 This will be a well seasoned quarter!</div>}
       {average == 2 && showScore && <div className="cooked-img"><img src="/Cooked2.svg" alt="Peter"/>2/5 This quarter will be a well-done steak!</div>}
       {average == 3 && showScore && <div className="cooked-img"><img src="/Cooked3.svg" alt="Peter"/>3/5 It's getting kinda spicy...</div>}
       {average == 4 && showScore && <div className="cooked-img"><img src="/Cooked4.svg" alt="Peter"/>4/5 You are cooked mate!</div>}
-      {average == 5 && showScore && <div className="cooked-img"><img src="/Cooked5.svg" alt="Peter"/>5/5 Cooked. Burnt. Toasted...</div>}
+      {average >= 5 && showScore && <div className="cooked-img"><img src="/Cooked5.svg" alt="Peter"/>5/5 Cooked. Burnt. Toasted...</div>}
     </div>
   )
 }
