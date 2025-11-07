37dbce380277b6423d48934c15d857dc8adc0310
Katie Li <133906958+katieli12@users.noreply.github.com>
2025-11-06 21:33:03 -0800

    visuals

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -9,7 +9,8 @@ export default function Main() {
         <img src="/Title.svg" alt="Peter's Cooked-O Meter" className="title-svg" />
       </div>
       <div className="info-text"> 
-        Enter professor first, then enter your class, and click "Calculate" to see how cooked you are! 
+        Enter professor first, then enter your class, and click "Calculate" to see how cooked you are!
+        <br />aka. how hard the course load will be 
       </div>
       <div className="button-row">
         <AddButton />
